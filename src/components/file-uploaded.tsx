82ff7620e0be9e4781058181091eb80d955e2947
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2024-06-25 09:57:53 -0600

    fixed blurred

--- src/components/file-uploaded.tsx
+++ src/components/file-uploaded.tsx
@@ -1,67 +1,61 @@
-import Image from "next/image";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { image as stackBlurImage } from "stackblur-canvas";
 
 interface FileUploadedHandlerProps {
-    file: File,
-    handleBlurChanges?: (value: number) => void,
-    sliderUpload?: (file: File, blur: number) => Promise<void>,
-    isSliderChanging?: boolean,
-    setSliderChanging?: (value: (((prevState: boolean) => boolean) | boolean)) => void
+    file: File;
+    handleBlurChanges?: (value: number) => void;
 }
 
 export default function FileUploadedHandler({
                                                 file,
                                                 handleBlurChanges,
-                                                sliderUpload
                                             }: FileUploadedHandlerProps) {
-
     const [blur, setBlur] = useState(0);
-    const timeoutId = useRef<NodeJS.Timeout | null>(null);
+    const url = URL.createObjectURL(file);
+    const imageRef = useRef<HTMLImageElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        const img = imageRef.current;
+        const canvas = canvasRef.current;
+
+        if (img && canvas) {
+            img.onload = () => {
+                canvas.width = 500;
+                canvas.height = 500;
+                const context = canvas.getContext("2d");
+                if (context) {
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    context.drawImage(img, 0, 0, canvas.width, canvas.height);
+                    stackBlurImage(img, canvas, blur*3.8);
+                }
+            };
+        }
+    }, [url, blur]);
 
     const handleBlurChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newBlur = parseInt(event.target.value);
         setBlur(newBlur);
-
-        if (handleBlurChanges) {
-            handleBlurChanges(newBlur);
-            if (timeoutId.current) {
-                clearTimeout(timeoutId.current);
-            }
-            timeoutId.current = setTimeout(() => {
-                sliderUpload && sliderUpload(file, newBlur);
-            }, 300);
-        }
-    }
-
-    const url = URL.createObjectURL(file);
+        handleBlurChanges && handleBlurChanges(newBlur);
+    };
 
     return (
-        <div>
-            <div style={{
-                overflow: 'hidden',
-                padding: '10px'
-            }}>
-                <Image
-                    style={{
-                        filter: `blur(${blur})`,
-                    }}
-                    src={url}
-                    alt="Uploaded file"
-                    width={500}
-                    height={500}
-                    className="rounded-2xl"
-                />
+        <div style={{ position: "relative", display: "inline-block" }}>
+            <div className="border-none relative m-auto max-w-full max-h-full rounded-3x">
+                <img ref={imageRef} src={url} alt="Uploaded file" style={{ display: "none" }} />
+                <canvas ref={canvasRef} />
             </div>
             <input
-                style={{ margin: '10px 0 20px 0' }}
                 type="range"
                 min="0"
                 max="64"
                 value={blur}
                 onChange={handleBlurChange}
+                style={{ marginTop: "40px" }}
             />
-
-            <p className="font-bold m-auto pb-8 text-white">Blur: {blur}px</p>
+            <p className="font-bold m-auto pb-8 text-white" style={{ zIndex: 2 }}>
+                Blur: {blur}px
+            </p>
         </div>
     );
 }
